c12a8eff6529f9ef7b42f2ab096b14abfd31ea5e
Railanna Fernandes <114159279+railannasf@users.noreply.github.com>
2023-01-17 14:00:46 +0000

    Comando de Decisão Aninhados

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const prompt = require ('prompt-sync') ();
 
-var nome = prompt ("Digite o seu nome:");
-var salario_bruto = parseFloat(prompt ("Digite o salário bruto:"));
+var nome = prompt ("Digite o seu nome: ");
+var salario_bruto = parseFloat(prompt ("Digite o salário bruto: "));
+var num = parseInt (prompt ("Digite o número de dependentes: "));
+var renda = salario_bruto/num;
 
-if (salario_bruto > 0 && salario_bruto <= 1903.38) {
-  var ir = salario_bruto * 0.05;
+if (renda >= 500)  {
+  if (salario_bruto > 0 && salario_bruto <= 1903.38) {
+    var ir = salario_bruto * 0.05;
 }else if (salario_bruto >1903.38 &&salario_bruto<= 2826.65){
   var ir = salario_bruto * 0.075;
 }else {
-  var ir= salario_bruto * 0.15;
+  var ir= salario_bruto * 0.15
 }
-var salario_liquido = salario_bruto - ir;
+  var salario_liquido = salario_bruto - ir;
 console.log ("Salário líquido R$", salario_liquido);
+
+} else {
+  var ir = 0
+  console.log ("A pessoa está insenta do imposto de renda.")
+}
+
--- readme.md
+++ readme.md
@@ -1,8 +1,13 @@
 Problema:
 Elabore um algoritmo para calcular o salário líquido de uma pessoa.
 Solicite ao usuário que digite o seu nome e o salário bruto.
-O imposto de renda a ser descontado do salário bruto deve se considerar as seguintes regras:
+Peça também para o usuário digitar a quantidade de dependentes.
+Calcule a renda da família per capta.
+Caso a  renda para cada membro da família seja menor que R$ 500,00 a pessoa ficará insenta de imposto de renda, ou seja, nao será calculado dentre as faixas salariais e seu valor será zero.
+Caso a renda para cada membro da famíla seja maior ou igual a R$ 500,00 o imposto de renda a ser descontado do salário bruto deve se considerar as seguintes regras:
+
 * salário bruto de (0,00 até 1.903,38):5%
 * salário bruto de (1.903,38 até 2.826,65):7,5%
-* salário bruto a partir de  ( 2.826,65):15%
+* salário bruto a partir de ( 2.826,65):15%
+  
 Ao final, mostre na tela o valor do salário líquido.
